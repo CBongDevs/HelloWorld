675de1d55fd6800102f087348974897fe11606c7
Andrew Thompson <mrwerdo331@me.com>
2020-04-20 13:55:07 +1000

    Click index to flash server time

--- finance-web/src/App.tsx
+++ finance-web/src/App.tsx
@@ -2,25 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+interface AppState {
+  status: string;
+}
+
+class App extends React.Component<{}, AppState> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = { status: '' };
+  }
+
+  async buttonClick() {
+    let request = await fetch('/api/ping');
+    let json = await request.json();
+    this.setState({
+      'status': json['time']
+    });
+  }
+
+  render(): JSX.Element {
+    return (
+    <div onClick={this.buttonClick.bind(this)} className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to React
+        <p key={Date.now()} id='status'>
+          {this.state.status}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-    </div>
-  );
+    </div>)
+  }
 }
 
 export default App;
