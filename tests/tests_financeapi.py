0b9ff298fcb47934ea709534cc74ccbac64aaef9
Andrew Thompson <mrwerdo331@me.com>
2020-04-25 20:21:04 +1000

    Ignore broken test

--- tests/tests_financeapi.py
+++ tests/tests_financeapi.py
@@ -3,6 +3,7 @@ import financeapi
 from unittest import mock
 
 class TestFinanceApiMethods(unittest.TestCase):
+    @unittest.skip("cannot set built-in attributes")
     def test_current_time(self):
         with mock.patch('financeapi.datetime.now', return_value='the current time'):
             model = financeapi.current_time()
