10577d844c87fc422ffd6a6c0c46a9f4269b847e
Andrew Thompson <mrwerdo331@me.com>
2020-04-25 19:48:15 +1000

    Added tests and app.json

---
+++ tests/tests_financeapi.py
@@ -0,0 +1,14 @@
+import unittest
+import financeapi
+from unittest import mock
+
+class TestFinanceApiMethods(unittest.TestCase):
+    def test_current_time(self):
+        with mock.patch('financeapi.datetime.now', return_value='the current time'):
+            model = financeapi.current_time()
+            self.assertEqual(model['time'], 'the current time')
+        
+    def test_database_url(self):
+        with mock.patch('financeapi.os.getenv', return_value='database-url'):
+            model = financeapi.current_time()
+            self.assertEqual(model['database_url'], 'database-url')
