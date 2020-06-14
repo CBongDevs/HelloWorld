3b7fa41d64dbd6c280e18ae38a45edd456ce95a7
Braydon Hall <40751395+nobrayner@users.noreply.github.com>
2020-06-15 08:16:56 +1000

    fix: Incorrect syntax in deploy.py

--- deploy.py
+++ deploy.py
@@ -100,8 +100,7 @@ def get_git_revision(base_path):
 
 def request_source_blob_endpoint(app):
     print('Requesting source blob endpoints...')
-    source_blob = requests.post(sources_api(app),
-                                headers=headers).json()['source_blob']
+    source_blob = requests.post(sources_api(app), headers=headers).json()['source_blob']
     get_url = source_blob['get_url']
     put_url = source_blob['put_url']
     return (get_url, put_url)
